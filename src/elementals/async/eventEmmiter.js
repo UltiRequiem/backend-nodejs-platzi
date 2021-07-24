cf83e645761612b101242d4b9fc1893392c52d51
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-24 13:24:51 -0500

    :art: Update

--- src/elementals/async/eventEmmiter.js
+++ src/elementals/async/eventEmmiter.js
@@ -10,9 +10,23 @@ class Logger extends EventEmmiter {
   }
 }
 
+class CustomLogger extends Logger {
+  execute(cb) {
+    console.log('A custom Logger to Try TabNine');
+    this.emit('Start');
+    cb();
+    console.log('Nice!');
+    this.emit('Finish');
+  }
+}
+
 const myLogger = new Logger();
+const mySecongLogger = new CustomLogger();
 
 myLogger.on('Start', () => console.log('Hi!'));
+mySecongLogger.on('Start', () => console.log('Hi!'));
+mySecongLogger.on('Finish', () => console.log('Chao!'));
 myLogger.on('Finish', () => console.log('Chao!'));
 
 myLogger.execute(() => console.log('Hi Execution!'));
+mySecongLogger.execute(() => console.log('Hi Tabnine!'));
--- src/elementals/async/promise.js
+++ src/elementals/async/promise.js
@@ -8,6 +8,8 @@ const helloPromises = () =>
     }, 500);
   });
 
+
+
 const printResolve = (promise) => {
   promise
     .then((msg) => msg.toUpperCase())
