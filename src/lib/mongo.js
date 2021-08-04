ad9abf86dec3553657836e22abebc4d843c5a11d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-04 14:10:49 -0500

    :art: Update

---
+++ src/lib/mongo.js
@@ -0,0 +1,32 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('../config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+src://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { usNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect((err) => {
+          if (err) {
+            reject(err);
+          }
+          console.log('Connected successfully to MongoDB!');
+          resolve(this.client.db(this.dbName));
+        });
+      });
+    }
+    return MongoLib.connection;
+  }
+}
+
+module.exports = MongoLib;
